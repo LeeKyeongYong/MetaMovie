7641882b61cfccbbd1c8d6a3da98b3301e1f18bd
이경용 <sleekydz86@naver.com>
2023-04-09 16:23:51 +0900

    jstl 등 기타라이브러리 추가
    jstl 등 기타라이브러리 추가

--- movieMbti/src/main/java/com/movie/meta/controller/MovieController.java
+++ movieMbti/src/main/java/com/movie/meta/controller/MovieController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MovieController {
-    @GetMapping("/hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
+
+    @RequestMapping(value = {"/", "/index.do"} , method = {RequestMethod.GET, RequestMethod.POST})
+    public String showMain(Model model) {
+        return "/home/main";
     }
 }
