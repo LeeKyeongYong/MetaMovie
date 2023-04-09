fa5fff05c82f2d35a8a3aa91e524cef993d6b781
이경용 <sleekydz86@naver.com>
2023-04-09 12:15:52 +0900

    환경셋팅 다시
    환경셋팅 다시

---
+++ movieMbti/src/main/java/com/movie/metaMovie/MetaMovieApplication.java
@@ -0,0 +1,13 @@
+package com.movie.metaMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaMovieApplication.class, args);
+	}
+
+}
---
+++ movieMbti/src/test/java/com/movie/metaMovie/MetaMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.metaMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
